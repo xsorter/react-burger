209533387233220898e08f88c809bd886fadfbcc
Oleksii Nelin <xsorter@gmail.com>
2020-05-10 17:36:08 +0300

    added total price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,9 @@ const orderSummary = (props) => {
       <h3>Your Order:</h3>
       <p>Burger with ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}$</strong>
+      </p>
       <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>
         CANCEL
