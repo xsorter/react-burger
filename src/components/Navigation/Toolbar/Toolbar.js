efba0dc20a290b93a53e46825c9ebfabe83f01cf
Oleksii Nelin <xsorter@gmail.com>
2020-05-22 22:36:52 +0300

    naigation items added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Toolbar.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
   <header className="Toolbar">
     <div>MENU</div>
 
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavigationItems></NavigationItems>
+    </nav>
   </header>
 );
 export default toolbar;
