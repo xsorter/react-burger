d47180a325bc4005b4a6c11cee98e74314edd166
Oleksii Nelin <xsorter@gmail.com>
2020-05-26 23:29:25 +0300

    sideDrawer added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Layout.sass";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
   <React.Fragment>
     <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </React.Fragment>
 );
